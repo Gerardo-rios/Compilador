ad607fdfd994d138692300f617cda5d91ba749e3
gerardo-rios-sf <gerardo.rios@staffingfuture.com>
2022-08-20 23:00:21 -0500

    se bugeo el commit xd

--- compilerLogic/views.py
+++ compilerLogic/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .controller.src import analizadorLexico 
 def index(request):
+    """"
+    Cargar los analizadores en chevere
+    Cuando uno sea elegido, se pasa a la vista de abajo para hacer el análisis
+    TAMBIEN PONER LA OPCION DE ANALISIS COMPLETO EN DONDE SE HARÁN LOS 3 ANALISIS 
+    """
     context = {
         'data': "Acá se debe cargar los 3 analizadores"
     }
     return render(request, 'analyzers/index.html', context)
 
 def lex_analyzer(request):
+    """
+    En este metodo se deben cargar los archivos para que sean analizados
+    Osea como en dropdown box para elegir el numero de archivo podrías er
+    en total son 6. 
+    DE IGUAL FORMA PARA LOS OTROS ANALIZADORES 
+    """
     return render(request, 'analyzers/lexiconAnalyzer.html')
 
 def results(request):
-    data = analizadorLexico.sendData()
+    """
+    ACÁ LLAMAS AL METODO DEL ANALIZADOR CORRESPONDIENTE, 
+    PASANDOLE EL NUMERO DE ARCHIVO EN ESTE CASO
+    Y EN ESTA VISTA SE CARGARÍAN LOS RESULTADOS
+    """
+    data = analizadorLexico.doAnalysis(request.fileNumber)
     return render(request, 'analyzers/result.html', {'data': data})
 
 
